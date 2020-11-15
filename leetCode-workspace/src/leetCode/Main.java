3034dd8e3657ca9c8b0d19fbae8ac5b63a40917e
LalitGoud11 <lalitgoud.devloper@gmail.com>
2020-11-15 12:54:35 -0600

    init commit

---
+++ leetCode-workspace/src/leetCode/Main.java
@@ -0,0 +1,18 @@
+package leetCode;
+
+public class Main {
+	
+	public static <R> R print( R print ){
+		return print;
+	}
+	
+	public static void main(String[] args){
+		
+		System.out.println(print(10));
+		System.out.println(print("101"));
+
+	}
+
+  
+    
+}
